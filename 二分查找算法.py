8ef370f88da385c0b63a6596658f76fcac63212d
wangdaodaodao <ahking0516@126.com>
2023-11-11 22:23:03 +0800

    2023

---
+++ 二分查找算法.py
@@ -0,0 +1,14 @@
+# alphabet = [chr(ord('a') + i) for i in range(26)]
+# print(alphabet)
+
+dic = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+goal = ['j']
+count = 0
+
+
+length = len(dic)
+
+for c in dic:
+    
